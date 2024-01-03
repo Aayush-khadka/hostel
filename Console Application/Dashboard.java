7cd0f7ac6bd9a70e7ea78cdffd329974ce9e0cf0
Aayush <khadkaaayush@proton.me>
2024-01-03 22:38:56 +0545

    Dashboard Updated

--- Console Application/Dashboard.java
+++ Console Application/Dashboard.java
@@ -3,7 +3,7 @@ public class Dashboard {
     public static void main(String[] args) 
     {
 
-        System.out.println("This is Dashboard");
+        System.out.println("WELCOME TO HOSTEL MANAGEMENT SYSTEM");
 
     }
     
