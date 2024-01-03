1f71cdda44ebabbdb997df022df702e38f8c9358
Aayush <118432407+Aayush-khadka@users.noreply.github.com>
2024-01-03 23:28:07 +0545

    Update Dashboard.java

--- Console Application/Dashboard.java
+++ Console Application/Dashboard.java
@@ -3,7 +3,7 @@ public class Dashboard {
     public static void main(String[] args) 
     {
 
-        System.out.println("DASH BOARD");
+        System.out.println("THIS IS thE FKING DASH BOARD");
 
     }
     
