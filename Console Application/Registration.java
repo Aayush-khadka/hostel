10251e3720e4803303d20af8d496c7aa236ddf65
Aayush <khadkaaayush@proton.me>
2024-01-05 17:32:42 +0545

    Added  Login CLI

--- Console Application/Registration.java
+++ Console Application/Registration.java
@@ -1,8 +1,19 @@
 import java.util.*;
-public class Registration {
-    public void registerUser() {
-        System.out.println(" This is  Hostel Registration PAGE");
-    }
 
-    
+public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = 0;
+
+        String[] f_name = new String[50];
+        String[] l_name = new String[50];
+
+        System.out.print("Enter your first name: ");
+        f_name[counter] = scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        l_name[counter] = scanner.nextLine();
+
+        System.out.println("Hello, " + f_name[counter] + " " + l_name[counter] + "!");
+    }
 }
